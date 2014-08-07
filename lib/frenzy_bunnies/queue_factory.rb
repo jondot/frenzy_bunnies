1a1fa171a88235d6a71debe856cae7c9adfd25c5
Daniel Farrell <danielfarrell76@gmail.com>
2014-08-06 20:13:12 -0400

    Support other exchange type and routing keys

--- lib/frenzy_bunnies/queue_factory.rb
+++ lib/frenzy_bunnies/queue_factory.rb
@@ -4,14 +4,19 @@ class FrenzyBunnies::QueueFactory
     @exchange = exchange
   end
 
-  def build_queue(name, prefetch, durable)
+  def build_queue(name, options)
+    durable = options[:durable]
+    prefetch = options[:prefetch]
+    type = options[:type] || :direct
+    routing_key = options[:routing_key] || name
+
     channel = @connection.create_channel
     channel.prefetch = prefetch
 
-    exchange = channel.exchange(@exchange, :type => :direct, :durable => durable)
+    exchange = channel.exchange(@exchange, :type => type, :durable => durable)
 
     queue = channel.queue(name, :durable => durable)
-    queue.bind(exchange, :routing_key => name)
+    queue.bind(exchange, :routing_key => routing_key)
     queue
   end
 end
